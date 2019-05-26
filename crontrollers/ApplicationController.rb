dd188ffe83bd17f0f81019d9729abc922f49c893
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-05-26 13:57:07 -0600

    finished application controller

--- crontrollers/ApplicationController.rb
+++ crontrollers/ApplicationController.rb
@@ -2,5 +2,32 @@ class ApplicationController < Sinatra::Base
     require 'bundler'
     Bundler.require
 
+    ActiveRecord::Base.establish_connection(
+        :adapter => 'postgresql',
+        :database => 'coffee_guys'
+
+
+    use Rack::MethodOverride
+    set :method_override, true
+
+
+
+    set :views, File.expand_path('../../views', __FILE__)
+    set :public_dir, File.expand_path('../../public', __FILE__)
+
+
+    get '/' do
+        redirect '/recipes'
+    end
+
+    get '/test' do
+    test_text = "A cup of coffee is all you need."
+    binding.pry
+    "pry has finished, #{test_text}"
+    end
+
+    get '*' do
+        halt 404
+    end
 
 end
