e930c30a18abab1f6aeec35de9f9a411bd8db850
Justin Atkins <justinatkins@Justins-MBP-2.attlocal.net>
2019-05-26 17:16:00 -0500

    added migrations.sql to db

---
+++ db/migrations.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS coffee_recipes;
+CREATE DATABASE coffee_recipes;
+
+--Connect to DB
+\c coffee_recipes
+
+CREATE TABLE users(
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(32),
+	password_digest VARCHAR(60)
+);
+
+CREATE TABLE recipes(
+	id SERIAL PRIMARY KEY,
+	recipe_name VARCHAR (255),
+	user_id INTEGER REFERENCES users(id) --foreign key
+);
